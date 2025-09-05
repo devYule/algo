c518f6e26e379d628a7f5bdf6294459a9b3eb0eb
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-05 17:53:26 +0900

    [level 3] Title: 최고의 집합, Time: 0.21 ms, Memory: 55.2 MB -BaekjoonHub

--- Java/프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ Java/프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -4,7 +4,7 @@ class Solution {
         if(base==0) return new int[] {-1};
         int add=s%n;
         int[] ret=new int[n];
-        for(int i=n-1; i>=0; i--) { ret[i]=base+(add-->0 ? 1 : 0); }
+        for(int i=n-1; i>=0; i--) ret[i]=base+(add-->0 ? 1 : 0);
         return ret;
     }
 }
