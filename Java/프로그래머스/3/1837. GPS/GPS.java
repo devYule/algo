4186cd6bdf399947f9fcd31f01a87c0f7a0460e8
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-07 21:20:08 +0900

    Update GPS.java

--- Java/프로그래머스/3/1837. GPS/GPS.java
+++ Java/프로그래머스/3/1837. GPS/GPS.java
@@ -8,7 +8,6 @@ class Solution {
     */
     // 오류가 있는 데이터의 수정 최소 횟수.
     // 수정 불가: -1
-    int[][] fld;
     int[][] adj;
     int V;
     int[] arr;
@@ -28,8 +27,7 @@ class Solution {
     }
     
     int getBest(int cur, int curIdx) {
-        if(arr.length-curIdx-1<fld[cur][goal]) return MAX;
-        if(curIdx==arr.length-1 && cur==goal) return 0;
+        if(curIdx==arr.length-1) return cur==goal ? 0 : MAX;
         if(memo[cur][curIdx]!=-1) return memo[cur][curIdx];
         
         int next=arr[curIdx+1];
@@ -44,26 +42,10 @@ class Solution {
     void init(int V, int[][] edge) {
         this.V=V;
         this.adj=new int[V][V];
-        this.fld=new int[V][V];
-        for(int i=0; i<V; i++) {
-            adj[i][i]=1;
-            Arrays.fill(fld[i], (int)1e9);
-            fld[i][i]=0;
-        }
+        for(int i=0; i<V; i++) adj[i][i]=1;
         for(int[] e: edge) {
             adj[e[0]][e[1]]=1;
             adj[e[1]][e[0]]=1;
-            fld[e[0]][e[1]]=1;
-            fld[e[1]][e[0]]=1;
-        }
-        
-        for(int k=0; k<V; k++) {
-            for(int i=0; i<V; i++) {
-                if(fld[i][k]>=(int)1e9) continue;
-                for(int j=0; j<V; j++) {
-                    fld[i][j]=Math.min(fld[i][j], fld[i][k]+fld[k][j]);
-                }
-            }
         }
     }
 }
