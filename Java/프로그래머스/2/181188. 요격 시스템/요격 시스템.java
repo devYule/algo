34d01ad9e0e4caa16531e64e358904c848d713da
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-07 19:08:32 +0900

    [level 2] Title: 요격 시스템, Time: 484.08 ms, Memory: 152 MB -BaekjoonHub

--- Java/프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ Java/프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,23 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        int N = targets.length;
-        int left = targets[0][0];
-        int right = targets[0][1];
-        int ret = 1;
+        Arrays.sort(targets, (a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+        int N=targets.length;
+        int right=targets[0][1];
+        int ret=1;
         for(int i=1; i<N; i++) {
             int[] cur = targets[i];
-            if(cur[0] < right) {
-                left = cur[0];
-                right = Math.min(right, cur[1]);
-            } else {
-                ret++;
-                left = cur[0];
-                right = cur[1];
-            }
+            if(cur[0]<right) right=Math.min(right, cur[1]);
+            else { right=cur[1]; ret++; }
         }
-        
         return ret;
     }
 }
