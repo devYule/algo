f95f330da4118121db165f93a9ec73e6d1504848
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-08 19:18:14 +0900

    [level 3] Title: 브라이언의 고민, Time: 32.31 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/1830. 브라이언의 고민/브라이언의 고민.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class Solution {
+    public String solution(String S) {
+        final String INV="invalid";
+        int[] cnt=new int[26];
+        boolean[] used=new boolean[26];
+        
+        for(int i=0; i<S.length(); i++) {
+            if(!((S.charAt(i)>='A' && S.charAt(i)<='Z') || (lower(S.charAt(i))))) return INV;
+            if(lower(S.charAt(i))) cnt[S.charAt(i)-'a']++;
+        }
+        List<int[]> border=new ArrayList<>();
+        
+        int i=0;
+        while(i<S.length()) {
+            int tmp=i;
+            while(i<S.length() && !lower(S.charAt(i))) i++;
+            if(i>=S.length()) {
+                border.add(new int[] {tmp, S.length()});
+                break;
+            }
+            
+            if(tmp==i && cnt[S.charAt(i)-'a']>2) return INV;
+            
+            char c=S.charAt(i);
+            if(lower(S.charAt(tmp)) && cnt[c-'a']!=2) return INV;
+            if(used[c-'a']) return INV;
+            if(cnt[c-'a']==2) {
+                if(tmp<i) border.add(new int[] {tmp, i});
+                int lo=i+1;
+                int end=S.indexOf(c, lo);
+                if(lower(S.charAt(lo)) || end==-1) return INV;
+                i=lo;
+                char inner='Z';
+                while(i<end) {
+                    if(inner!='Z' && i>lo && !lower(S.charAt(i)) && !lower(S.charAt(i-1))) return INV;
+                    if(S.charAt(i)!=c && lower(S.charAt(i))) {
+                        if(inner=='Z') inner=S.charAt(i);
+                        if(inner!=S.charAt(i) || inner==c) return INV;
+                        if(lower(S.charAt(i-1)) || lower(S.charAt(i+1))) return INV;
+                    }
+                    i++;
+                }
+                if(lower(S.charAt(i-1))) return INV;
+                if(inner!='Z') {
+                    if(used[inner-'a'] || cnt[inner-'a']==0) return INV;
+                    used[inner-'a']=true;
+                    cnt[inner-'a']=0;
+                }
+                
+                cnt[c-'a']-=2;
+                border.add(new int[] {lo, end});
+                i=end+1;
+            } else {
+                if(tmp<i) border.add(new int[] {tmp, i-1});
+                int lo=i-1;
+                while(i<S.length() && S.charAt(i)==c) {
+                    if(i==S.length()-1 || lower(S.charAt(i+1)) || lower(S.charAt(i-1))) return INV;
+                    cnt[c-'a']--;
+                    i+=2;
+                }
+                if(lo+1==i || i>S.length()) return INV;
+                border.add(new int[] {lo, i});
+            }
+            if(cnt[c-'a']!=0) return INV;
+            used[c-'a']=true;
+        }
+        
+        StringBuilder sb=new StringBuilder();
+        for(int[] b: border) {
+            String tk=S.substring(b[0], b[1]);
+            if(tk.isEmpty()) continue;
+            sb.append(" ");
+            sb.append(tk);
+        }
+        return sb.toString().replaceAll("[a-z]", "").replaceAll(" {2,}", " ").trim();
+    }
+    
+    boolean lower(char c) {
+        return c>='a' && c<='z';
+    }
+}
