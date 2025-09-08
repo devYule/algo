5d986c45dafeb9818fffe4c7f68152c310445c5e
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-08 21:26:13 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 93.6 MB -BaekjoonHub

--- Java/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ Java/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,46 +1,18 @@
-import java.util.*;
 class Solution {
-    // 서버 현황
-    int[] server;
-    // i 시점에 추가된 서버 대수
-    int[] added;
-    
-    int[] players;
-    int n, m, k;
     public int solution(int[] players, int m, int k) {
-        this.n = players.length;
-		this.m = m;
-        this.k = k;
-        this.players = players;
-        this.server = new int[n];
-        this.added = new int[n];
-        Arrays.fill(added, -1);
-		server[0] = 1;
-        int ret = server(0);
-        return ret;
-    }
-    
-	int server(int idx) {
-        if(idx < 0) return 0;
-        if(idx == n) return 0;
-        if(added[idx] != -1) return added[idx];
-        
-        // k 번째 전에 추가된 서버 대수 구하기.
-        int dim = server(idx - k);
-        // 현재 유효 서버 대수
-        server[idx] = (idx == 0 ? 0 : server[idx - 1]) - dim;
-        
-        added[idx] = 0;
-        // 서버 추가하기
-        int need = players[idx] / m;
-        if(need > server[idx]) {
-            added[idx] = need - server[idx];
-            server[idx] = need;
+        int set=0;
+        int[] add=new int[24];
+        int ret=0;
+        for(int i=0; i<24; i++) {
+            if(i>=k) set-=add[i-k];
+            int need=players[i]/m;
+            if(set<need) {
+                int added=need-set;
+                set+=added;
+                ret+=added;
+                add[i]=added;
+            }
         }
-        
-        // 다음호출
-        int afterAdded = server(idx + 1);
-        
-        return added[idx] + afterAdded;
+        return ret;
     }
 }
