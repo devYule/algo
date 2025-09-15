e52ae38fa7641a82c0de4da0d535a13b250c024c
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-15 23:20:26 +0900

    [level 3] Title: 가장 먼 노드, Time: 36.75 ms, Memory: 114 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/README.md
+++ Java/프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 38.57 ms
+메모리: 114 MB, 시간: 36.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 04일 16:14:39
+2025년 09월 15일 23:20:24
 
 ### 문제 설명
 
--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,31 @@
 import java.util.*;
 class Solution {
-    List<Integer>[] adj;
-    int V;
-    public int solution(int n, int[][] edge) {
-        init(n+1, edge);
-        
-        return bfs(1);
-    }
-    
-    int bfs(int start) {
-        int[] dist=new int[V];
-        Queue<Integer> q=new LinkedList<>();
-        Arrays.fill(dist, -1);
-        dist[start]=0;
-        q.add(start);
-        
-        while(!q.isEmpty()) {
-            int cur=q.poll();
-            
-            for(int next: adj[cur]) {
-                if(dist[next]!=-1) continue;
-                dist[next]=dist[cur]+1;
-                q.add(next);
-            }
-        }
-        
-        Arrays.sort(dist);
-        int max=dist[dist.length-1];
-        int ret=0;
-        for(int i=V-1; i>=0; i--) {
-            if(dist[i]!=max) break;
-            ret++;
-        }
-        return ret;
-    }
-    
     @SuppressWarnings("unchecked")
-    void init(int V, int[][] edge) {
-        this.V=V;
-        this.adj=new ArrayList[V];
-        for(int i=1; i<V; i++) adj[i]=new ArrayList<>();
-        for(int[] e: edge) {
-            adj[e[0]].add(e[1]);
-            adj[e[1]].add(e[0]);
+    public int solution(int n, int[][] edge) {
+        List<Integer>[] adj=new ArrayList[n+1];
+        for(int i=1; i<=n; i++) adj[i]=new ArrayList<>();
+        for(int[] e: edge) { adj[e[0]].add(e[1]); adj[e[1]].add(e[0]); }
+        
+        Deque<Integer> q=new ArrayDeque<>();
+        q.add(1);
+        boolean[] vis=new boolean[n+1];
+        int round=0;
+        vis[1]=true;
+        while(!q.isEmpty()) {
+            int size=q.size();
+            for(int i=0; i<size; i++) {
+                int cur=q.poll();
+                
+                for(int next: adj[cur]) {
+                    if(!vis[next]) {
+                        q.addLast(next);
+                        vis[next]=true;
+                    }
+                }
+            }
+            round++;
+            if(q.isEmpty()) return size;
         }
+        return -1;
     }
 }
