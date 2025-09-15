6b7d4f9da94468049395ddbb9301eb9dc766dcd1
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-09-15 23:11:22 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 85.1 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,29 +1,34 @@
+import java.util.*;
 class Solution {
-    final int MAX=(int)1e9;
-    String target;
     public int solution(String begin, String target, String[] words) {
-        this.target=target;
-        int ret=find(begin, 0, words);
-        return ret==MAX ? 0 : ret;
-    }
-    
-    int find(String cur, long vis, String[] words) {
-        if(cur.equals(target)) return 0;
-        if(Long.bitCount(vis)==words.length) return MAX;
-        int ret=MAX;
-        for(int i=0; i<words.length; i++) {
-            if((vis&1<<i)!=0) continue;
-            String w=words[i];
-            int ci=0;
-            int wi=0;
-            int diff=0;
-            while(ci<cur.length() && wi<w.length()) {
-                if(cur.charAt(ci)!=w.charAt(wi)) diff++;
-                if(diff>1) break;
-                ci++; wi++;
+        Deque<String> q=new ArrayDeque<>();
+        q.add(begin);
+        
+        boolean[] used=new boolean[words.length];
+        int round=0;
+        while(!q.isEmpty()) {
+            int size=q.size();
+            for(int i=0; i<size; i++) {
+                String cur=q.poll();
+                if(cur.equals(target)) return round;
+                for(int j=0; j<words.length; j++) {
+                    if(used[j]) continue;
+                    int ci=0, wi=0;
+                    int diff=0;
+                    while(ci<cur.length() && wi<words[j].length()) {
+                        if(cur.charAt(ci)!=words[j].charAt(wi)) diff++;
+                        if(diff>1) break;
+                        ci++; wi++;
+                    }
+                    if(diff==1) {
+                        q.addLast(words[j]);
+                        used[j]=true;
+                    }
+                }
             }
-            if(diff==1) ret=Math.min(ret, find(w, vis|1<<i, words)+1);
+            if(q.isEmpty()) return 0;
+            round++;
         }
-        return ret;
+        return 0;
     }
 }
