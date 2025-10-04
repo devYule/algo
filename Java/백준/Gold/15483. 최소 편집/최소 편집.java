afa213d1dc0e95c37b8e43978ba350107cef0547
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-04 17:27:59 +0900

    [Gold III] Title: 최소 편집, Time: 132 ms, Memory: 18372 KB -BaekjoonHub

---
+++ Java/백준/Gold/15483. 최소 편집/최소 편집.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			bw.write(
+				String.valueOf(
+					new Main().resolve(
+						br.readLine(),
+						br.readLine()
+					)
+				)
+			);
+			bw.flush();
+		}
+	}
+
+	int resolve(String a, String b) {
+		int n=a.length(), m=b.length();
+		int[][] dp=new int[n+1][m+1];
+
+		for(int i=0; i<=n; i++) dp[i][0]=i;
+		for(int i=0; i<=m; i++) dp[0][i]=i;
+
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=m; j++) {
+				int cost=a.charAt(i-1)==b.charAt(j-1) ? 0 : 1;
+				dp[i][j]=Math.min(dp[i-1][j-1]+cost, Math.min(dp[i-1][j]+1, dp[i][j-1]+1));
+			}
+		}
+
+		return dp[n][m];
+	}
+
+}
