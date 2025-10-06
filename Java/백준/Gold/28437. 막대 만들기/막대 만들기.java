54816d8ac9e0049356ae377fe30e0352087b882d
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-06 17:01:56 +0900

    [Gold III] Title: 막대 만들기, Time: 812 ms, Memory: 55644 KB -BaekjoonHub

---
+++ Java/백준/Gold/28437. 막대 만들기/막대 만들기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			int n=Integer.parseInt(br.readLine());
+			int[] s=Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+			int q=Integer.parseInt(br.readLine());
+			int[] l=Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+
+
+			bw.write(
+				String.valueOf(
+					new Main().resolve(
+						n, s, q, l
+					)
+				)
+			);
+			bw.flush();
+		}
+	}
+
+	String resolve(int n, int[] A, int q, int[] L) {
+		int max=Arrays.stream(L).max().orElseThrow();
+		int[] dp=new int[max+1];
+		for(int a: A) if(a<=max) dp[a]=1;
+
+		for(int i=2; i<=max; i++) {
+			int sqrt=(int) Math.sqrt(i);
+			for(int j=1; j<=sqrt; j++) {
+				if(i%j!=0) continue;
+				int bu=dp[i/j];
+				dp[i]+=dp[j];
+				if(j!=1 && j!=i/j) dp[i]+=bu;
+			}
+		}
+		List<Integer> ret=new ArrayList<>();
+		for(int l: L) ret.add(dp[l]);
+		return ret.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining(" "));
+	}
+
+}
