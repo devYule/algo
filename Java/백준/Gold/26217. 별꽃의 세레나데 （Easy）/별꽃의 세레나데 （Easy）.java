bee7853cbfb6590f962340ddb5cc4e34cc6ad3d0
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-09 12:57:56 +0900

    [Gold V] Title: 별꽃의 세레나데 (Easy), Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ Java/백준/Gold/26217. 별꽃의 세레나데 （Easy）/별꽃의 세레나데 （Easy）.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			bw.write(
+				String.valueOf(
+					new Main().resolve(
+						Integer.parseInt(br.readLine())
+					)
+				)
+			);
+			bw.flush();
+		}
+	}
+
+	double resolve(int n) {
+		double ret=1d;
+		for(double i=1; i<n; i++) {
+			ret+=n/i;
+		}
+		return ret;
+	}
+
+}
