46bfe6ecfad9668e1142e6ce0605195f1fb234ba
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-15 14:09:12 +0900

    [Gold IV] Title: LCS 2, Time: 156 ms, Memory: 20324 KB -BaekjoonHub

--- Java/백준/Gold/9252. LCS 2/LCS 2.java
+++ Java/백준/Gold/9252. LCS 2/LCS 2.java
@@ -22,9 +22,8 @@ public class Main {
 		int[][] dp=new int[n+1][m+1];
 		for(int i=1; i<=n; i++) {
 			for(int j=1; j<=m; j++) {
-				int dist=0;
-				if(a.charAt(i-1)==b.charAt(j-1)) dist=1;
-				dp[i][j]=Math.max(dp[i-1][j-1]+dist, Math.max(dp[i-1][j], dp[i][j-1]));
+				if(a.charAt(i-1)==b.charAt(j-1)) dp[i][j]=dp[i-1][j-1]+1;
+				else dp[i][j]=Math.max(dp[i-1][j-1], Math.max(dp[i-1][j], dp[i][j-1]));
 			}
 		}
 
