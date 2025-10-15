a88df933080c9a5010575f6df24ebc241b7a0e29
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-15 13:31:05 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18936 KB -BaekjoonHub

--- Java/백준/Gold/9251. LCS/LCS.java
+++ Java/백준/Gold/9251. LCS/LCS.java
@@ -8,7 +8,7 @@ public class Main {
 			bw.write(
 				String.valueOf(
 					new Main().resolve(
-						br.readLine(),
+						br.readLine(), 
 						br.readLine()
 					)
 				)
@@ -22,11 +22,11 @@ public class Main {
 		int[][] dp=new int[n+1][m+1];
 		for(int i=1; i<=n; i++) {
 			for(int j=1; j<=m; j++) {
-				int cost=a.charAt(i-1)==b.charAt(j-1) ? 1 : 0;
-				dp[i][j]=Math.max(dp[i-1][j-1]+cost, Math.max(dp[i-1][j], dp[i][j-1]));
+				int dist=0;
+				if(a.charAt(i-1)==b.charAt(j-1)) dist=1;
+				dp[i][j]=Math.max(dp[i-1][j-1]+dist, Math.max(dp[i-1][j], dp[i][j-1]));
 			}
 		}
 		return dp[n][m];
 	}
-
 }
