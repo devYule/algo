8450694db4af14b2a9e3be3b320c4c70275dee80
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-20 14:22:03 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 164 ms, Memory: 19564 KB -BaekjoonHub

--- Java/백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ Java/백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -6,13 +6,13 @@ public class Main {
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int n=Integer.parseInt(br.readLine());
-			int[] nums=new int[n];
-			for(int i=0; i<n; i++) nums[i]=Integer.parseInt(br.readLine());
+			int[] cases=new int[n];
+			for(int i=0; i<n; i++) cases[i]=Integer.parseInt(br.readLine());
 
 			bw.write(
 				String.valueOf(
 					new Main().resolve(
-						n, nums
+						n, cases
 					)
 				)
 			);
@@ -20,11 +20,10 @@ public class Main {
 		}
 	}
 
-	final int MOD=1_000_000_009;
-	String resolve(int n, int[] nums) {
-		int max=Arrays.stream(nums).max().orElse(0);
+	String resolve(int n, int[] cases) {
+		final int MOD=1_000_000_009;
+		int max=Arrays.stream(cases).max().orElse(0);
 		int[][] dp=new int[max+1][4];
-
 		dp[1][1]=1;
 		dp[2][2]=1;
 		dp[3][1]=1;
@@ -34,16 +33,18 @@ public class Main {
 		for(int i=4; i<=max; i++) {
 			for(int j=1; j<=3; j++) {
 				for(int k=1; k<=3; k++) {
-					if(j==k) continue;
-					dp[i][j]=(dp[i][j]+dp[i-j][k])%MOD;
+					if(j==k || i-j<0 || i<j) continue;
+					dp[i][j]=(dp[i][j]+(dp[i-j][k]%MOD))%MOD;
 				}
 			}
 		}
-
-		List<Integer> ret=new ArrayList<>();
-		for(int num: nums) ret.add((int)((dp[num][1]+(long)dp[num][2]+dp[num][3])%MOD));
-
-		return ret.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining("\n"));
+		dp[0][1]=dp[0][2]=dp[0][3]=0;
+		StringBuilder sb=new StringBuilder();
+		for(int i=0; i<n; i++) {
+			if(i!=0) sb.append("\n");
+			sb.append(String.valueOf(((dp[cases[i]][1]+dp[cases[i]][2])%MOD+dp[cases[i]][3])%MOD));
+		}
+		return sb.toString();
 	}
 
 }
