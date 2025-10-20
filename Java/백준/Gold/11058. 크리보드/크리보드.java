d56550741fabf8fdff294fe0d0e3826d17320242
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-20 17:13:37 +0900

    [Gold V] Title: 크리보드, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- Java/백준/Gold/11058. 크리보드/크리보드.java
+++ Java/백준/Gold/11058. 크리보드/크리보드.java
@@ -18,10 +18,10 @@ public class Main {
 
 	long resolve(int n) {
 		long[] dp=new long[n+1];
-		for(int i=1; i<=Math.min(6, n); i++) dp[i]=i;
-		for(int i=7; i<=n; i++) {
-			dp[i]=dp[i-1]+1;
-			for(int j=i-3; j>=1; j--) {
+		for(int i=1; i<=Math.min(5, n); i++) dp[i]=i;
+
+		for(int i=4; i<=n; i++) {
+			for(int j=i-3; j>=0; j--) {
 				dp[i]=Math.max(dp[i], dp[j]*(i-j-1));
 			}
 		}
