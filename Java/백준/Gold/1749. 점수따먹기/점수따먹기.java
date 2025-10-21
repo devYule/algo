f430810416742220ac3e2ef20c1f51ab2ac49416
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-21 13:22:01 +0900

    [Gold IV] Title: 점수따먹기, Time: 356 ms, Memory: 21916 KB -BaekjoonHub

--- Java/백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ Java/백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -29,38 +29,22 @@ public class Main {
 	}
 
 	int resolve(int n, int m, int[][] s) {
-		int[][] sum=new int[n][m];
-		sum[0][0]=s[0][0];
-		for(int i=1; i<m; i++) sum[0][i]=sum[0][i-1]+s[0][i];
-		for(int i=1; i<n; i++) sum[i][0]=sum[i-1][0]+s[i][0];
-
-		for(int i=1; i<n; i++) {
-			for(int j=1; j<m; j++) {
-				sum[i][j]=(s[i][j]+sum[i-1][j]+sum[i][j-1])-sum[i-1][j-1];
-			}
-		}
-
-		int[][] best=new int[n][m];
-		best[0][0]=sum[0][0];
-		for(int i=1; i<m; i++) best[0][i]=Math.max(sum[0][i], s[0][i]);
-		for(int i=1; i<n; i++) best[i][0]=Math.max(sum[i][0], s[i][0]);
-
-		for(int i=1; i<n; i++) {
-			for(int j=1; j<m; j++) {
-				best[i][j]=sum[i][j];
-				if(i>0) best[i][j]=Math.max(best[i][j], best[i-1][j]);
-				if(j>0) best[i][j]=Math.max(best[i][j], best[i][j-1]);
-				if(i>0 && j>0) best[i][j]=Math.max(best[i][j], best[i-1][j-1]);
-				for(int k=0; k<i; k++) {
-					for(int l=0; l<j; l++) {
-						if(l==0) best[i][j]=Math.max(best[i][j], sum[i][j]-sum[k][j]);
-						if(k==0) best[i][j]=Math.max(best[i][j], sum[i][j]-sum[i][l]);
-						best[i][j]=Math.max(best[i][j], (sum[i][j]-sum[k][j]-sum[i][l])+sum[k][l]);
-					}
+		int ret=Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			int[] sum=new int[m];
+			int[] dp=new int[m];
+			for(int j=i; j<n; j++) {
+				for(int k=0; k<m; k++) {
+					sum[k]+=s[j][k];
+				}
+				dp[0]=sum[0];
+				ret=Math.max(ret, dp[0]);
+				for(int k=1; k<m; k++) {
+					dp[k]=Math.max(dp[k-1]+sum[k], sum[k]);
+					ret=Math.max(ret, dp[k]);
 				}
 			}
 		}
-
-		return best[n-1][m-1];
+		return ret;
 	}
 }
