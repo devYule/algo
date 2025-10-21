87c14b68841f014872a5710a284c6e6c1846f58c
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-21 15:46:01 +0900

    [Silver I] Title: 점프, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- Java/백준/Silver/1890. 점프/점프.java
+++ Java/백준/Silver/1890. 점프/점프.java
@@ -25,35 +25,18 @@ public class Main {
 		}
 	}
 
-	int n, map[][];
-	long[][] memo;
-	final int[] rg={0, 1};
-	final int[] cg={1, 0};
 	long resolve(int n, int[][] map) {
-		this.n=n; this.map=map;
-		this.memo=new long[n][n];
-		for(int i=0; i<n; i++) Arrays.fill(memo[i], -1);
-		this.memo[n-1][n-1]=1;
-		return find(0, 0);
-	}
-
-	long find(int y, int x) {
-		if(memo[y][x]!=-1) return memo[y][x];
-		int cost=map[y][x];
-		if(cost==0) return memo[y][x]=0;
-		long ret=0;
-		for(int i=0; i<2; i++) {
-			int ny=y+(rg[i]*cost);
-			int nx=x+(cg[i]*cost);
-			if(valid(ny, nx)) {
-				ret+=find(ny, nx);
+		long[][] dp=new long[n][n];
+		dp[0][0]=1;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				int jump=map[i][j];
+				if(jump==0) continue;
+				if(i+jump<n) dp[i+jump][j]+=dp[i][j];
+				if(j+jump<n) dp[i][j+jump]+=dp[i][j];
 			}
 		}
-		return memo[y][x]=ret;
-	}
-
-	boolean valid(int y, int x) {
-		return y>=0 && x>=0 && y<n && x<n;
+		return dp[n-1][n-1];
 	}
 
 }
