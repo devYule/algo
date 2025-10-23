651748eb3e7b6e15242c4d5edb6990b425390f1a
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-23 16:29:03 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 140 ms, Memory: 16708 KB -BaekjoonHub

--- Java/백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ Java/백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -6,7 +6,11 @@ public class Main {
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int n=Integer.parseInt(br.readLine());
-			int[] nums=Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+			int[] nums=new int[n];
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) {
+				nums[i]=Integer.parseInt(st.nextToken());
+			}
 
 			bw.write(
 				String.valueOf(
@@ -20,29 +24,42 @@ public class Main {
 	}
 
 	String resolve(int n, int[] nums) {
-		int[] dp=new int[n];
-		Arrays.fill(dp, 1);
-		for(int i=0; i<n; i++) {
-			for(int j=i+1; j<n; j++) {
-				if(nums[i]<nums[j]) {
-					dp[j]=Math.max(dp[j], dp[i]+1);
-				}
+		int[] tails=new int[n];
+		int[] loci=new int[n];
+		int ti=1;
+		tails[0]=nums[0];
+		for(int i=1; i<n; i++) {
+			int cur=nums[i];
+			if(tails[ti-1]<cur) {
+				tails[ti]=cur;
+				loci[i]=ti++;
+			} else {
+				int loc=loc(tails, cur, ti-1);
+				tails[loc]=cur;
+				loci[i]=loc;
 			}
 		}
-
 		List<Integer> ret=new ArrayList<>();
-		int max=Arrays.stream(dp).max().orElseThrow();
-		int rm=max;
-
+		int li=ti-1;
 		for(int i=n-1; i>=0; i--) {
-			if(dp[i]==max) {
+			if(loci[i]==li) {
+				li--;
 				ret.add(nums[i]);
-				max--;
 			}
 		}
 		Collections.reverse(ret);
 
-		return rm + "\n" + ret.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining(" "));
+		return ti + "\n" + ret.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining(" "));
+	}
+
+	int loc(int[] tails, int target, int hi) {
+		int lo=0;
+		while(lo<=hi) {
+			int mid=(lo+hi)>>>1;
+			if(tails[mid]>=target) hi=mid-1;
+			else lo=mid+1;
+		}
+		return lo;
 	}
 
 }
