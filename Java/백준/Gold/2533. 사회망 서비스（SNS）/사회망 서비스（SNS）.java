e46b488101a7660c5b239919133f9315059d239a
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-28 12:07:35 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 976 ms, Memory: 272652 KB -BaekjoonHub

--- Java/백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ Java/백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -6,6 +6,7 @@ public class Main {
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int n=Integer.parseInt(br.readLine());
+
 			int[][] edge=new int[n-1][2];
 			for(int i=0; i<n-1; i++) {
 				StringTokenizer st=new StringTokenizer(br.readLine());
@@ -24,79 +25,51 @@ public class Main {
 		}
 	}
 
-	int V, first[], nxt[], node[], eni;
+	int head[], nxt[], to[], ei;
+	int n, memo[][];
 
 	int resolve(int n, int[][] edge) {
-		n++;
-		int start=init(n, edge);
-		int[][] memo=new int[n][2];
-		for(int i=0; i<n; i++) Arrays.fill(memo[i], -1);
+		init(n, edge);
+		this.memo=new int[n+1][2];
+		for(int i=1; i<=n; i++) Arrays.fill(memo[i], -1);
 
-		Queue<Integer> q=new ArrayDeque<>();
-		int[] parent=new int[V];
-		int[] order=new int[V];
-		q.add(start);
-		parent[start]=start;
-		int oi=0;
-		while(!q.isEmpty()) {
-			int cur=q.poll();
-			order[oi++]=cur;
+		return Math.min(dfs(1, 1, -1), dfs(1, 0, -1));
+	}
 
-			for(int ni=first[cur]; ni!=-1; ni=nxt[ni]) {
-				int next=node[ni];
-				if(parent[next]!=0) continue;
-				parent[next]=cur;
-				q.add(next);
-			}
+	int dfs(int cur, int ps, int parent) {
+		if(memo[cur][ps]!=-1) return memo[cur][ps];
+		int ret=ps;
+		for(int i=head[cur]; i!=-1; i=nxt[i]) {
+			int next=to[i];
+			if(next==parent) continue;
+
+			if(ps==1) ret+=Math.min(dfs(next, 0, cur), dfs(next, 1, cur));
+			else ret+=dfs(next, 1, cur);
 		}
-
-		for(int i=oi-1; i>=0; i--) {
-			int cur=order[i];
-
-			int curOn=1;
-			int curOff=0;
-
-			for(int ni=first[cur]; ni!=-1; ni=nxt[ni]) {
-				int next=node[ni];
-				if(parent[cur]==next) continue;
-				curOff+=memo[next][1];
-				curOn+=Math.min(memo[next][0], memo[next][1]);
-			}
-			memo[cur][1]=curOn;
-			memo[cur][0]=curOff;
-		}
-
-		return Math.min(memo[start][0], memo[start][1]);
+		return memo[cur][ps]=ret;
 	}
 
 
-	int init(int v, int[][] edge) {
-		this.V=v;
+	void init(int n, int[][] edge) {
+		this.n=n;
+		this.head=new int[n+1];
+		Arrays.fill(head, -1);
+		int E=edge.length;
+		this.nxt=new int[E*2];
+		this.to=new int[E*2];
+		this.ei=0;
 
-		this.first=new int[v];
-		Arrays.fill(first, -1);
-		int m=edge.length;
-		this.nxt=new int[m*2];
-		this.node=new int[m*2];
-		this.eni=0;
-
-		int[] ind=new int[V];
 		for(int[] e: edge) {
-			node[eni]=e[1];
-			nxt[eni]=first[e[0]];
-			first[e[0]]=eni++;
+			int a=e[0], b=e[1];
+			to[ei]=b;
+			nxt[ei]=head[a];
+			head[a]=ei++;
 
-			node[eni]=e[0];
-			nxt[eni]=first[e[1]];
-			first[e[1]]=eni++;
-
-			ind[e[0]]++; ind[e[1]]++;
+			to[ei]=a;
+			nxt[ei]=head[b];
+			head[b]=ei++;
 		}
 
-		for(int i=1; i<V; i++) {
-			if(ind[i]==1) return i;
-		}
-		return -1;
 	}
 
 }
