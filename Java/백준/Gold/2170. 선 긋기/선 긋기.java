06773573801c7e42175a050cf1041439a958e271
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-10-31 14:54:42 +0900

    [Gold V] Title: 선 긋기, Time: 1912 ms, Memory: 362112 KB -BaekjoonHub

--- Java/백준/Gold/2170. 선 긋기/선 긋기.java
+++ Java/백준/Gold/2170. 선 긋기/선 긋기.java
@@ -25,23 +25,28 @@ public class Main {
 		}
 	}
 
-	int resolve(int n, int[][] p) {
-		Arrays.sort(p, (a, b) -> a[0]==b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+	int resolve(int n, int[][] fromTo) {
+		List<int[]> event=new ArrayList<>();
+    for(int ft[]: fromTo) {
+      int from=Math.min(ft[0], ft[1]);
+      int to=Math.max(ft[0], ft[1]);
+      event.add(new int[] {from, 1});
+      event.add(new int[] {to, -1});
+    }
 
-		int ret=0;
-		int ls=p[0][0];
-		int rs=p[0][1];
-		for(int i=1; i<n; i++) {
-			int[] c=p[i];
-			if(c[0]>rs) {
-				ret+=rs-ls;
-				ls=c[0];
-				rs=c[1];
-			} else if(c[1]>rs) {
-				rs=c[1];
-			}
-		}
-		return ret+(rs-ls);
+    event.sort((a, b) -> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]);
+
+    int ret=0;
+    int connect=0;
+    int start=0;
+
+    for(int i=0; i<event.size(); i++) {
+      int e[]=event.get(i);
+      connect+=e[1];
+      if(e[1]==-1 && connect==0) ret+=e[0]-start;
+      if(e[1]==1 && connect==1) start=e[0];
+    }
+    return ret;
 	}
 
 }
