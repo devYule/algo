61ff26da12a8f4a8f19feb10381642996f506c66
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-07 09:32:24 +0900

    [Gold IV] Title: 알고스팟, Time: 116 ms, Memory: 14772 KB -BaekjoonHub

--- Java/백준/Gold/1261. 알고스팟/알고스팟.java
+++ Java/백준/Gold/1261. 알고스팟/알고스팟.java
@@ -6,9 +6,9 @@ public class Main {
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			StringTokenizer st=new StringTokenizer(br.readLine());
-
 			int m=Integer.parseInt(st.nextToken());
 			int n=Integer.parseInt(st.nextToken());
+
 			int[][] map=new int[n][m];
 			for(int i=0; i<n; i++) {
 				String line=br.readLine();
@@ -29,31 +29,35 @@ public class Main {
 	}
 
 	int resolve(int n, int m, int[][] map) {
-		final int[] rg={-1, 0, 1, 0};
-		final int[] cg={0, 1, 0, -1};
-
-		Deque<int[]> q=new ArrayDeque<>();
+		PriorityQueue<int[]> q=new PriorityQueue<>((a, b) -> a[0]-b[0]);
 		int[][] dist=new int[n][m];
 		for(int i=0; i<n; i++) Arrays.fill(dist[i], (int)1e9);
-		q.add(new int[] {0, 0});
+		q.add(new int[] {0, 0, 0});
 		dist[0][0]=0;
+
+		int[] rg={-1, 0, 1, 0};
+		int[] cg={0, 1, 0, -1};
 		while(!q.isEmpty()) {
-			int[] curs=q.poll();
-			int y=curs[0];
-			int x=curs[1];
-			int cost=dist[y][x];
+			int[] as=q.poll();
+			int y=as[1];
+			int x=as[2];
+			int cost=as[0];
+
+			if(dist[y][x]!=cost) continue;
+
 			for(int i=0; i<4; i++) {
 				int ny=y+rg[i];
 				int nx=x+cg[i];
 				if(ny>=0 && nx>=0 && ny<n && nx<m) {
-					if(cost+map[ny][nx]<dist[ny][nx]) {
-						dist[ny][nx]=cost+map[ny][nx];
-						if(map[ny][nx]==0) q.addFirst(new int[] {ny, nx});
-						else q.addLast(new int[] {ny, nx});
+					int nextCost=cost+map[ny][nx];
+					if(dist[ny][nx]>nextCost) {
+						dist[ny][nx]=nextCost;
+						q.add(new int[] {nextCost, ny, nx});
 					}
 				}
 			}
 		}
+
 		return dist[n-1][m-1];
 	}
 
