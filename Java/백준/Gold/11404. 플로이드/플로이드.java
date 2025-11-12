6052ee96d7f0093f08f7e904756082ac21509a3b
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-12 12:50:32 +0900

    [Gold IV] Title: 플로이드, Time: 388 ms, Memory: 46084 KB -BaekjoonHub

--- Java/백준/Gold/11404. 플로이드/플로이드.java
+++ Java/백준/Gold/11404. 플로이드/플로이드.java
@@ -1,54 +1,63 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
-
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static StringTokenizer st;
-    private static int N;
-    private static int m;
-    private static int[][] graph;
-    private static int MAX = Integer.MAX_VALUE >>> 1;
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        graph = new int[N + 1][N + 1];
+			int n=Integer.parseInt(br.readLine());
+			int m=Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (i == j) continue;
-                graph[i][j] = MAX;
-            }
-        }
+			int[][] edge=new int[m][3];
+			for(int i=0; i<m; i++) {
+				StringTokenizer st=new StringTokenizer(br.readLine());
+				edge[i][0]=Integer.parseInt(st.nextToken());
+				edge[i][1]=Integer.parseInt(st.nextToken());
+				edge[i][2]=Integer.parseInt(st.nextToken());
+			}
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int f = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            graph[f][t] = Math.min(graph[f][t], v);
-        }
+			bw.write(
+				String.valueOf(
+					new Main().resolve(
+						n, m, edge
+					)
+				)
+			);
+			bw.flush();
+		}
+	}
 
-        for (int z = 1; z <= N; z++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    graph[i][j] = Math.min(graph[i][j], graph[i][z] + graph[z][j]);
-                }
-            }
-        }
-
-        String r = "";
-        for (int i = 1; i <= N; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 1; j <= N; j++) {
-                sb.append((graph[i][j] == MAX ? 0 : graph[i][j]) + " ");
-            }
-            r += sb.toString().trim() + "\n";
-        }
-        System.out.println(r.trim());
-    }
+	String resolve(int n, int m, int[][] edge) {
+		int[][] adj=new int[n+1][n+1];
+		for(int i=1; i<=n; i++) {
+			Arrays.fill(adj[i], (int)1e9);
+			adj[i][i]=0;
+		}
+		int ei=0;
+		for(int[] e: edge) {
+			int a=e[0], b=e[1], d=e[2];
+			adj[a][b]=Math.min(adj[a][b], d);
+		}
 
 
+		for(int k=1; k<=n; k++) {
+			for(int i=1; i<=n; i++) {
+				if(adj[i][k]==(int)1e9) continue;
+				for(int j=1; j<=n; j++) {
+					if(adj[k][j]==(int)1e9) continue;
+					adj[i][j]=Math.min(adj[i][j], adj[i][k]+adj[k][j]);
+				}
+			}
+		}
+
+		StringBuilder sb=new StringBuilder();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(j!=1) sb.append(" ");
+				sb.append(adj[i][j]==(int)1e9 ? 0 : adj[i][j]);
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 }
-
