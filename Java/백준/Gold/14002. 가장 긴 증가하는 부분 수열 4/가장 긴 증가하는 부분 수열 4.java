b5a5d809fb407ed130f08839bf94b784cd15ce01
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-17 13:18:07 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 140 ms, Memory: 16468 KB -BaekjoonHub

--- Java/백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ Java/백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -8,6 +8,7 @@ public class Main {
 			int n=Integer.parseInt(br.readLine());
 			int[] nums=new int[n];
 			StringTokenizer st=new StringTokenizer(br.readLine());
+
 			for(int i=0; i<n; i++) {
 				nums[i]=Integer.parseInt(st.nextToken());
 			}
@@ -25,35 +26,37 @@ public class Main {
 
 	String resolve(int n, int[] nums) {
 		int[] tails=new int[n];
-		int[] loci=new int[n];
+		int[] tis=new int[n];
+		Arrays.fill(tis, -1);
 		int ti=1;
 		tails[0]=nums[0];
+		tis[0]=0;
 		for(int i=1; i<n; i++) {
-			int cur=nums[i];
-			if(tails[ti-1]<cur) {
-				tails[ti]=cur;
-				loci[i]=ti++;
+			int num=nums[i];
+			if(num>tails[ti-1]) {
+				tis[i]=ti;
+				tails[ti++]=num;
 			} else {
-				int loc=loc(tails, cur, ti-1);
-				tails[loc]=cur;
-				loci[i]=loc;
+				int loc=tails(0, ti-1, num, tails);
+				tails[loc]=num;
+				tis[i]=loc;
 			}
 		}
-		List<Integer> ret=new ArrayList<>();
-		int li=ti-1;
-		for(int i=n-1; i>=0; i--) {
-			if(loci[i]==li) {
-				li--;
-				ret.add(nums[i]);
-			}
-		}
-		Collections.reverse(ret);
 
-		return ti + "\n" + ret.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining(" "));
+		List<Integer> res=new ArrayList<>();
+		int ret=ti--;
+		for(int i=n-1; i>=0; i--) {
+			if(tis[i]==ti) {
+				res.add(nums[i]);
+				ti--;
+			}
+		}
+		Collections.reverse(res);
+
+		return ret + "\n" + res.stream().map(String::valueOf).collect(java.util.stream.Collectors.joining(" "));
 	}
 
-	int loc(int[] tails, int target, int hi) {
-		int lo=0;
+	int tails(int lo, int hi, int target, int[] tails) {
 		while(lo<=hi) {
 			int mid=(lo+hi)>>>1;
 			if(tails[mid]>=target) hi=mid-1;
