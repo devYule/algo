f45d8b6f2472096beaf2361f4297094e1e7215b9
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-21 12:10:36 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 484 ms, Memory: 64948 KB -BaekjoonHub

--- Java/백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ Java/백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -32,35 +32,71 @@ public class Main {
 		}
 	}
 
-	long adj[][];
-	long resolve(int n, int m, int[][] edge, int a, int b) {
+	int head[], to[], next[], wt[], n;
+	int MAX=(int)1e9;
+	int resolve(int n, int m, int[][] edge, int a, int b) {
 		init(n, edge);
 
-		for(int k=1; k<=n; k++) {
-			for(int i=1; i<=n; i++) {
-				for(int j=1; j<=n; j++) {
-					adj[i][j]=Math.min(adj[i][j], adj[i][k]+adj[k][j]);
+		int[] dist1=dijk(1, a);
+		int[] dist2=dijk(1, b);
+		int[] dist3=dijk(a, b);
+		int[] dist4=dijk(a, n);
+		int[] dist5=dijk(b, n);
+
+		if(dist1==null || dist2==null || dist3==null || dist4==null || dist5==null) return -1;
+		return Math.min(
+			dist1[a]+dist3[b]+dist5[n],
+			dist2[b]+dist3[b]+dist4[n]
+		);
+
+	}
+
+	int[] dijk(int f, int t) {
+		int[] dist=new int[n+1];
+		Arrays.fill(dist, MAX);
+		PriorityQueue<int[]>q=new PriorityQueue<>((a, b) -> a[0]-b[0]);
+		dist[f]=0;
+		q.add(new int[] {0, f});
+		while(!q.isEmpty()) {
+			int[] as=q.poll();
+			int cost=as[0];
+			int a=as[1];
+			if(dist[a]!=cost) continue;
+			for(int ni=head[a]; ni!=-1; ni=next[ni]) {
+				int b=to[ni];
+				int nextCost=cost+wt[ni];
+				if(dist[b]>nextCost) {
+					dist[b]=nextCost;
+					q.add(new int[] {nextCost, b});
 				}
 			}
 		}
 
-		long ret=Math.min(
-			adj[1][a]+adj[a][b]+adj[b][n],
-			adj[1][b]+adj[b][a]+adj[a][n]
-		);
-		return ret>=(long)1e10 ? -1L : ret;
+		return dist[t]==MAX ? null : dist;
+
 	}
 
 	void init(int n, int[][] edge) {
-		adj=new long[n+1][n+1];
-		for(int i=0; i<=n; i++) {
-			Arrays.fill(adj[i], (long)1e10);
-			adj[i][i]=0;
-		}
+		int E=edge.length;
+		head=new int[n+1];
+		Arrays.fill(head, -1);
+		to=new int[E*2];
+		next=new int[E*2];
+		wt=new int[E*2];
+		this.n=n;
+
+		int ei=0;
 		for(int[] e: edge) {
 			int a=e[0], b=e[1], d=e[2];
-			adj[a][b]=d;
-			adj[b][a]=d;
+			wt[ei]=d;
+			to[ei]=b;
+			next[ei]=head[a];
+			head[a]=ei++;
+
+			wt[ei]=d;
+			to[ei]=a;
+			next[ei]=head[b];
+			head[b]=ei++;
 		}
 	}
 }
