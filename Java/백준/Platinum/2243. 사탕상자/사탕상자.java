38e078dc4d3626b2be4dafe3544fd09a912d3c7a
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-21 13:56:58 +0900

    [Platinum V] Title: 사탕상자, Time: 652 ms, Memory: 56536 KB -BaekjoonHub

--- Java/백준/Platinum/2243. 사탕상자/사탕상자.java
+++ Java/백준/Platinum/2243. 사탕상자/사탕상자.java
@@ -28,12 +28,12 @@ public class Main {
 	}
 
 	String resolve(int n, int[][] queries) {
-		Seg seg=new Seg();
+		Fenwick fw=new Fenwick();
 		StringBuilder sb=new StringBuilder();
 		boolean ln=false;
 		for(int[] q: queries) {
 			if(q[0]==2) {
-				seg.update(q[1], q[2]);
+				fw.update(q[1], q[2]);
 			} else {
 				if(ln) sb.append("\n");
 				ln=true;
@@ -41,42 +41,37 @@ public class Main {
 				int lo=1, hi=1_000_000;
 				while(lo<=hi) {
 					int mid=(lo+hi)>>>1;
-					int find=seg.query(mid);
+					int find=fw.query(mid);
 					if(find>=target) hi=mid-1;
 					else lo=mid+1;
 				}
 				sb.append(lo);
-				seg.update(lo, -1);
+				fw.update(lo, -1);
 			}
 		}
 		return sb.toString();
 	}
 
-	class Seg {
+	class Fenwick {
 		int n=1_000_001;
-		int tree[]=new int[4_000_004];
+		int tree[]=new int[1_000_001];
 		
-		void update(int idx, int acc) {
-			update(idx, acc, 0, n-1, 1);
+		void update(int idx, int value) {
+			while(idx<n) {
+				tree[idx]+=value;
+				idx+=idx&(-idx);
+			}
 		}
 
-		int update(int idx, int value, int l, int r, int node) {
-			if(l>idx || r<idx) return tree[node];
-			if(l==r) return tree[node]+=value;
-			int mid=(l+r)>>>1;
-			return tree[node]=update(idx, value, l, mid, node*2)+update(idx, value, mid+1, r, node*2+1);
+		int query(int idx) {
+			int ret=0;
+			while(idx>0) {
+				ret+=tree[idx];
+				idx-=idx&(-idx);
+			}
+			return ret;
 		}
 
-		int query(int r) {
-			return query(1, r, 0, n-1, 1);
-		}
-
-		int query(int bl, int br, int l, int r, int node) {
-			if(l>br || r<bl) return 0;
-			if(l>=bl && r<=br) return tree[node];
-			int mid=(l+r)>>>1;
-			return query(bl, br, l, mid, node*2)+query(bl, br, mid+1, r, node*2+1);
-		}
 
 	}
 
