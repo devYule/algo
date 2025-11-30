cbe4ac4d73d39b6a4b8283ef5caf4c2aeefea69b
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-11-30 12:19:16 +0900

    [Gold III] Title: 세 용액, Time: 752 ms, Memory: 16944 KB -BaekjoonHub

---
+++ Java/백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			int n=Integer.parseInt(br.readLine());
+			int[] nums=new int[n];
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) nums[i]=Integer.parseInt(st.nextToken());
+
+			bw.write(
+				String.valueOf(
+					new Main().resolve(
+						n, nums
+					)
+				)
+			);
+			bw.flush();
+		}
+	}
+
+	// -24 -6 -3 -2 61 98 100
+	String resolve(int n, int[] nums) {
+		Arrays.sort(nums);
+		int[] rets=new int[3];
+		long ret=Long.MAX_VALUE;
+		for(int i=0; i<n-1; i++) {
+			for(int j=i+1; j<n; j++) {
+				long two=nums[i]+nums[j];
+				int lo=j+1, hi=n-1;
+				long best=Long.MAX_VALUE;
+				int bm=-1;
+				while(lo<=hi) {
+					int mid=(lo+hi)>>>1;
+					long three=two+nums[mid];
+					if(Math.abs(three)<best) {
+						best=Math.abs(three);
+						bm=mid;
+					}
+					if(three>0) hi=mid-1; 
+					else if(three<0) lo=mid+1;
+					else break;
+				}
+				if(ret>best) {
+					ret=best;
+					rets[0]=nums[i];
+					rets[1]=nums[j];
+					rets[2]=nums[bm];
+				}
+			}
+		}
+		Arrays.sort(rets);
+		return rets[0] + " " + rets[1] + " " + rets[2];
+	}
+
+}
