70cb99b73236e80e959610911be364346426d8a4
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-12-04 12:59:24 +0900

    [Gold V] Title: 1학년, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- Java/백준/Gold/5557. 1학년/1학년.java
+++ Java/백준/Gold/5557. 1학년/1학년.java
@@ -5,11 +5,17 @@ public class Main {
 		try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
+			int n=Integer.parseInt(br.readLine());
+			int[] nums=new int[n];
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) {
+				nums[i]=Integer.parseInt(st.nextToken());
+			}
+
 			bw.write(
 				String.valueOf(
 					new Main().resolve(
-						Integer.parseInt(br.readLine()),
-						Arrays.stream(br.readLine().split("\\s")).mapToInt(Integer::parseInt).toArray()
+						n, nums
 					)
 				)
 			);
@@ -17,22 +23,21 @@ public class Main {
 		}
 	}
 
+	long[][] memo;
+	int n, nums[];
+
 	long resolve(int n, int[] nums) {
-		long[] dp=new long[21];
-		dp[nums[0]]=1;
 
+		long[][] dp=new long[n-1][21];
+		dp[0][nums[0]]=1;
 		for(int i=1; i<n-1; i++) {
-			int num=nums[i];
-			long[] newDp=new long[21];
+			int cur=nums[i];
 			for(int j=0; j<21; j++) {
-				if(dp[j]!=0) {
-					if(j-num>=0) newDp[j-num]+=dp[j];
-					if(j+num<=20) newDp[j+num]+=dp[j];
-				}
+				if(dp[i-1][j]==0) continue;
+				if(j+cur<=20) dp[i][j+cur]+=dp[i-1][j];
+				if(j-cur>=0) dp[i][j-cur]+=dp[i-1][j];
 			}
-			dp=newDp;
 		}
-		return dp[nums[n-1]];
+		return dp[n-2][nums[n-1]];
 	}
-
 }
