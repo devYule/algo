22e83b5ce2b48a2dca290f10ea8106ed4aca4daa
Cho Hyunmin <145819864+devYule@users.noreply.github.com>
2025-12-18 12:02:55 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 311056 KB -BaekjoonHub

--- Java/백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ Java/백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -6,17 +6,19 @@ public class Main {
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int n=Integer.parseInt(br.readLine());
-			int[][] jobs=new int[n][2];
+			int[] T=new int[n];
+			int[] P=new int[n];
+
 			for(int i=0; i<n; i++) {
 				StringTokenizer st=new StringTokenizer(br.readLine());
-				jobs[i][0]=Integer.parseInt(st.nextToken());
-				jobs[i][1]=Integer.parseInt(st.nextToken());
+				T[i]=Integer.parseInt(st.nextToken());
+				P[i]=Integer.parseInt(st.nextToken());
 			}
 
 			bw.write(
 				String.valueOf(
 					new Main().resolve(
-						n, jobs
+						n, T, P
 					)
 				)
 			);
@@ -24,11 +26,13 @@ public class Main {
 		}
 	}
 
-	long resolve(int n, int[][] jobs) {
-		long[] dp=new long[n+1];
+	int resolve(int n, int[] T, int[] P) {
+		int[] dp=new int[n+1];
 		for(int i=n-1; i>=0; i--) {
-			dp[i]=dp[i+1];
-			if(i+jobs[i][0]<dp.length) dp[i]=Math.max(dp[i], dp[i+jobs[i][0]]+jobs[i][1]);
+			if(i+T[i]-1<n) {
+				dp[i]=dp[i+T[i]]+P[i];
+			}
+			dp[i]=Math.max(dp[i], dp[i+1]);
 		}
 		return dp[0];
 	}
